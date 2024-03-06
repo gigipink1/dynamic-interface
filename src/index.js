60ad5f776ad5143200ea70dde0b76d337a581b40
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2024-03-06 15:38:25 +0000

    Add basic HTML and CSS file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-function badCode() {
-    
-};
+import './style.css';
 
-badCode();
+function makeDrop(divName, className) {
+    const menu = document.querySelector(`${className}`);
+    const Title = document.querySelector(`${divName}`);
+    Title.addEventListener('click', () => {
+        if(menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+            menu.classList.add('showing');
+        }
+        else {
+            menu.classList.remove('showing');
+            menu.classList.add('hidden')
+        }
+        
+    })
+
+}
+
+makeDrop('.menu', '.dropdown');
